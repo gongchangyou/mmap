34b3300c7747e1840ac8914c0a0a5c4ce0f3d50c
gongchangyou <gongchangyou@58.com>
2022-06-03 22:19:01 +0800

    thread test

--- src/test/java/com/mouse/mmap/MmapApplicationTests.java
+++ src/test/java/com/mouse/mmap/MmapApplicationTests.java
@@ -19,30 +19,69 @@ class MmapApplicationTests {
 
     int size = str.getBytes().length;
 
+    MappedByteBuffer buffer;
+
+    @Test
+    public void threadTest() {
+        val t1 = new Thread(() -> {
+            try {
+                writeWithMap();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        t1.start();
+
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        val t2 = new Thread(() -> {
+                log.info("is Loaded in physical memory: {}",buffer.isLoaded());  //只是一个提醒而不是guarantee
+                log.info("capacity {}",buffer.capacity());
+                byte[] bytes = new byte[size];
+
+                //read the buffer
+                for (int i = 0; i < bytes.length; i++)
+                {
+                    bytes[i] = buffer.get(i);
+//                log.info("get {}", buffer.get());
+                }
+
+                val str = new String(bytes);
+                log.info("str= {} currentThread={}", str, Thread.currentThread());
+        });
+        t2.start();
+
+    }
+
     @Test
     public void writeWithMap() throws IOException {
-        try (RandomAccessFile file = new RandomAccessFile(new File("src/main/resources/big.www.flydean.com"), "rw"))
+        try (RandomAccessFile file = new RandomAccessFile(new File("src/main/resources/testfile"), "rw"))
         {
             //get Channel
             FileChannel fileChannel = file.getChannel();
             //get mappedByteBuffer from fileChannel
-            MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, 1024 );
+            buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, 1024 );
             // check buffer
             log.info("is Loaded in physical memory: {}",buffer.isLoaded());  //只是一个提醒而不是guarantee
             log.info("capacity {}",buffer.capacity());
             //write the content
             buffer.put(str.getBytes());
+            log.info("str= {} currentThread={}", str, Thread.currentThread());
         }
     }
 
     @Test
     public void readWithMap() throws IOException {
-        try (RandomAccessFile file = new RandomAccessFile(new File("src/main/resources/big.www.flydean.com"), "r"))
+        try (RandomAccessFile file = new RandomAccessFile(new File("src/main/resources/testfile"), "r"))
         {
             //get Channel
             FileChannel fileChannel = file.getChannel();
             //get mappedByteBuffer from fileChannel
-            MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
             // check buffer
             log.info("is Loaded in physical memory: {}",buffer.isLoaded());  //只是一个提醒而不是guarantee
             log.info("capacity {}",buffer.capacity());
@@ -56,7 +95,7 @@ class MmapApplicationTests {
             }
 
             val str = new String(bytes);
-            log.info("str= {}", str);
+            log.info("str= {} currentThread={}", str, Thread.currentThread());
         }
     }
 
